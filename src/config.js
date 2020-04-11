d63b99f5001d05d30052e35d11fff3917479a9a5
harik2000 <hari@Haris-MacBook-Pro.local>
2020-04-11 11:38:11 -0700

    Setting up react app

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_zBNpkd9LU",
+      APP_CLIENT_ID: "60o2uqha1u39r7ucbb90lfqc8c",
+      IDENTITY_POOL_ID: "us-east-2:780c32cd-9472-40d5-8a54-9519bd7804df"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Don't Flake</h1>
+        <p>Keep your friends accountable</p>
+      </div>
+    </div>
+  );
+}
